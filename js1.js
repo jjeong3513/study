dff5ecd330fdeddde91590f6cd954a537106a0b9
jjeong3513 <102217713+jjeong3513@users.noreply.github.com>
2022-05-22 17:22:20 +0900

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,29 @@
+ var a ;
+ let b ;
+ //const c;
+
+ let user=50;
+ let userTelnumber;
+ let userAddress
+ let num=50; //숫자형 데이터
+ let str='자바스크립트'; 
+ let num1='2022';  // '' 안에 적혀 있으면 문자형 데이터
+ let value1=2022+'2023'; //NaN 타입 숫자
+ console.log(typeof value1);
+ console.log(typeof value2);
+ 
+ let str1 = 2022;
+ let str2 = '안녕'+2022+'\n년'; //+는 연결연산자. 더하기의 의미가 아님
+ /* 템플릿 리터럴 백틱 '*/ 
+ let str3 = '안녕하세요 올    해는 ${str1} 년입니다.'
+ console.log(str2)
+
+ /* boolean 블리언 */
+ 
+ let isOn=true;
+ let isOff=false;
+ console.log(isOn)
+ console.log(typeof isOff)
+
+ let js1;
+ console.log(typeof js1)
---
+++ js2.js
@@ -0,0 +1,14 @@
+/* 자바스크립트에서 거짓이라고 취급하는 값*/
+/* 0, 90, null, undefinded, NaN,'' */
+
+
+let user = {
+    name:'Kim',
+    age:50,
+    address:'seoul',
+    tel:'010'
+}
+
+console.log(user)
+console.log(user.name)
+// console.log(user.address)
